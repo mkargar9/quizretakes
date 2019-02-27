0f203f7e0f654f510b576638b34250deaf2a98ac
Maryam K <36210937+mkargar9@users.noreply.github.com>
2019-02-27 14:46:43 -0500

    Add files via upload
    added another test

--- quizscheduleTest.java
+++ quizscheduleTest.java
@@ -2,13 +2,21 @@ package quizretakes;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+
+import javax.xml.parsers.ParserConfigurationException;
+
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.time.LocalDate;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.xml.sax.SAXException;
+
 import static org.hamcrest.CoreMatchers.*;
 
 public class quizscheduleTest 
@@ -18,16 +26,19 @@ public class quizscheduleTest
 	LocalDate startSkip; //for courseBean course
 	LocalDate endSkip; //for courseBean course
 	courseBean course; 
-	
+	LocalDate today  = LocalDate.now();
+	LocalDate endDay = today.plusDays(new Long(14));
+	String courseID = "swe437";
+	   
 	@Before
 	public void setUp() throws Exception
 	{
-		//sets up data for testNullPointerException()
+		//sets up data 
 		startSkip = LocalDate.of(2019, 1, 21); 
 		endSkip = LocalDate.of(2019, 1, 25);
 		quizList = new quizzes();
 		retakesList = new retakes();
-		course = new courseBean("swe437", "Software Testing", "14", startSkip, endSkip, "/var/www/CS/webapps/offutt/WEB-INF/data/");
+		course = new courseBean(courseID, "Software Testing", "14", startSkip, endSkip, "/var/www/CS/webapps/offutt/WEB-INF/data/");
 	}
 
 	@After
@@ -66,8 +77,10 @@ public class quizscheduleTest
 		// Create a stream to hold the output
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		PrintStream ps = new PrintStream(baos);
+		
 		// IMPORTANT: Save the old System.out!
 		PrintStream old = System.out;
+		
 		// Tell Java to use your special stream
 		System.setOut(ps);
 		
@@ -77,11 +90,63 @@ public class quizscheduleTest
 		// Put things back
 		System.out.flush();
 		System.setOut(old);
+		
 		// Show what happened (need to use the toString() method)
 		assertEquals(baos.toString(), "Foofoofoo!");
 		
 		//another way to test a string for containing information
 		assertThat(baos.toString(), both(endsWith("!")).and(containsString("Foo")));
 	}
+	
+	/** 
+	 * This method tests correct printing correct today date and end date 
+	 */
+	@Test
+	public void testRetakeOutput()
+	{
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(out);
+		PrintStream old = System.out;
+		System.setOut(ps);
+		
+		quizschedule.printQuizScheduleForm(quizList, retakesList, course);
+		
+		System.out.flush();
+		System.setOut(old);
+		
+		String output = out.toString();
+		
+		//test prints out correct today date "Today is ...."
+		assertThat(output, CoreMatchers.containsString(today.getDayOfWeek() + ", " + today.getMonth() + " " + today.getDayOfMonth()));
+		
+		//tests prints out correct end date "scheduling quizzes until ..."
+		assertThat(output, CoreMatchers.containsString(endDay.getDayOfWeek() + ", " + endDay.getMonth() + " " + endDay.getDayOfMonth()));
+		
+	}
 
+	/**
+	 * This method tests the header print statements (first print statements)
+	 */
+	@Test
+	public void testHeader()
+	{
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(out);
+		PrintStream old = System.out;
+		System.setOut(ps);
+		
+		quizschedule.printQuizScheduleForm(quizList, retakesList, course);
+		
+		System.out.flush();
+		System.setOut(old);
+		
+		String output = out.toString();
+		
+		//tests header output print statements 
+		assertThat(output, CoreMatchers.containsString("GMU quiz retake scheduler for class " + course.getCourseTitle()));
+		assertThat(output, CoreMatchers.containsString("You can sign up for quiz retakes within the next two weeks. "));
+		assertThat(output, CoreMatchers.containsString("Enter your name (as it appears on the class roster), "));
+		assertThat(output, CoreMatchers.containsString("then select which date, time, and quiz you wish to retake from the following list."));
+	}
+	
 }
